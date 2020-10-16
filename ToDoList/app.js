397fd60e0db0cc29384c7b7cac4b1c81d4e8e748
Linda Marko <lindam@smpi.org>
2020-10-16 16:01:23 -0700

    Change day formay to use JS, and added form.

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -10,22 +10,16 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
     
     var today = new Date();
-    var weekday = new Array(7);
-    // var currentDay = today.getDay()
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "Weekend";
-    // } else {
-    //     day = "Weekday";
-    // }
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
 
-    var day = weekday[today.getDay()];
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    }
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    
 
     res.render('list', {kindOfDay: day});
 });
