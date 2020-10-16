2b2535dd69f7db90b3a46dddc47f385a5e36cc43
Linda Marko <lindam@smpi.org>
2020-10-16 16:54:36 -0700

    Added New item function to todolist

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -4,8 +4,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var toDoItems = [];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     
@@ -19,13 +22,16 @@ app.get("/", function(req, res) {
 
     var day = today.toLocaleDateString("en-US", options);
 
-    
-
-    res.render('list', {kindOfDay: day});
+    res.render('list', {kindOfDay: day, newlistItem: toDoItems});
 });
 
+app.post("/", function(req, res) {
+    var toDoItem = req.body.toDoItem;
 
-
+    toDoItems.push(toDoItem);
+    // toDoItems.push("<li>" + toDoItem + "</li>");
+    res.redirect("/");
+});
 
 app.listen(3000, function() {
     console.log("Server is running on Port 3000.");
