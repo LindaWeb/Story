55503854f21abc6b64e1ae0b5a6b3e04aafccb5e
Linda Marko <lindam@smpi.org>
2020-10-17 16:33:54 -0700

    Added styles to ToDoList

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -9,6 +9,7 @@ var toDoItems = ["Buy Food", "Cook Food", "Eat Food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     
