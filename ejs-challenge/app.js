642e810913d3c126349c9405d4c36432d7a002a4
Linda Marko <lindam@smpi.org>
2020-10-20 15:20:02 -0700

    Challanges 1-4

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -16,9 +16,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res){
+    res.render("home", {homeContent:homeStartingContent});
+});
 
+app.post("/", function(req, res) {
 
-
+});
 
 
 
@@ -31,3 +35,4 @@ app.use(express.static("public"));
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+-
